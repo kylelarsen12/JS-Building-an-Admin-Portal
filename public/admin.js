9673aa662891b7bfc32f0c8628ee557225a7972a
unknown <kylelarsen12@gmail.com>
2022-06-04 08:00:06 -0400

    complete assignment

--- public/admin.js
+++ public/admin.js
@@ -1,2 +1,34 @@
-
 // Your Code Here
+async function main() {
+  let response = await fetch("http://localhost:3001/listBooks");
+  let books = await response.json();
+  books.forEach(makeBook);
+}
+
+function makeBook(book) {
+  let root = document.querySelector("#root");
+
+  let li = document.createElement("li");
+  li.textContent = book.title;
+
+  let inputNum = document.createElement("input");
+  inputNum.value = book.quantity;
+
+  let saveButton = document.createElement("button");
+  saveButton.textContent = "Save";
+  saveButton.addEventListener("click", () => {
+    fetch("http://localhost:3001/updateBook", {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        id: book.id,
+        quantity: inputNum.value,
+      }),
+    });
+  });
+
+  li.append(inputNum, saveButton);
+  root.append(li);
+}
+
+main();
